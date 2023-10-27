31877974d7a6077eceb5dc76c57abe6caa2bb76b
unknown <j.mendezquesada18@gmail.com>
2023-10-27 11:19:36 -0600

    update code to only send a list of messges

--- sbqueue/__init__.py
+++ sbqueue/__init__.py
@@ -9,12 +9,12 @@ from azure.servicebus import ServiceBusMessage
 NAMESPACE_CONNECTION_STR = os.environ.get("TARGET_TOPIC_CONNECTION_STRING")
 TOPIC_NAME = os.environ.get("TARGET_TOPIC_NAME")
 
-async def send_single_message(sender):
+#async def send_single_message(sender):
     # Create a Service Bus message
-    message = ServiceBusMessage("Single Message")
+#    message = ServiceBusMessage("Single Message")
     # send the message to the topic
-    await sender.send_messages(message)
-    print("Sent a single message")
+#    await sender.send_messages(message)
+#    print("Sent a single message")
 
 async def send_a_list_of_messages(sender):
     # Create a list of messages
@@ -23,21 +23,21 @@ async def send_a_list_of_messages(sender):
     await sender.send_messages(messages)
     print("Sent a list of 5 messages")
 
-async def send_batch_message(sender):
+#async def send_batch_message(sender):
     # Create a batch of messages
-    async with sender:
-        batch_message = await sender.create_message_batch()
-        for _ in range(10):
-            try:
+#    async with sender:
+#        batch_message = await sender.create_message_batch()
+#        for _ in range(10):
+#            try:
                 # Add a message to the batch
-                batch_message.add_message(ServiceBusMessage("Message inside a ServiceBusMessageBatch"))
-            except ValueError:
+#                batch_message.add_message(ServiceBusMessage("Message inside a ServiceBusMessageBatch"))
+#            except ValueError:
                 # ServiceBusMessageBatch object reaches max_size.
                 # New ServiceBusMessageBatch object can be created here to send more data.
-                break
+#                break
         # Send the batch of messages to the topic
-        await sender.send_messages(batch_message)
-    print("Sent a batch of 10 messages")
+#        await sender.send_messages(batch_message)
+#    print("Sent a batch of 10 messages")
 
 async def run():
     # create a Service Bus client using the connection string
@@ -48,11 +48,11 @@ async def run():
         sender = servicebus_client.get_topic_sender(topic_name=TOPIC_NAME)
         async with sender:
             # Send one message
-            await send_single_message(sender)
+         #   await send_single_message(sender)
             # Send a list of messages
             await send_a_list_of_messages(sender)
             # Send a batch of messages
-            await send_batch_message(sender)
+         #   await send_batch_message(sender)
 
 
 
